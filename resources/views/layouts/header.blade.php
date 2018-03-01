a7accc53ff0bf8455b6ab1c2bd6ae4bf21d0c827
NgPhuongThuan <nguyenphuongthuan95@gmail.com>
2018-03-01 19:54:08 +0700

    add meta tag google

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -3,6 +3,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
+    <meta name="google-site-verification" content="97tOhFLSmYU1hYxIB-A9btNpH_tGN72oggNW4z4-my8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
