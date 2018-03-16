2c741d1090597d5383d56c210302c077197e8866
NgPhuongThuan <nguyenphuongthuan95@gmail.com>
2018-03-16 16:44:43 +0700

    Change something

---
+++ tests/Feature/CreatePostsTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Post;
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class CreatePostsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function an_authenticated_user_can_create_a_post()
+    {
+        $this->actingAs(factory(User::class)->create());
+
+        $post = factory(Post::class)->create();
+
+        $this->post('/posts', $post->toArray());
+
+        $this->get('/posts/' . $post->id)
+            ->assertSee($post->title)
+            ->assertSee($post->body);
+    }
+}
