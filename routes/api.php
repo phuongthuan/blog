1ea3dc9100c97fdcc812e949e16d0918d3acad1f
NgPhuongThuan <nguyenphuongthuan95@gmail.com>
2018-04-14 11:27:18 +0700

    Make testcase for POST Api

--- routes/api.php
+++ routes/api.php
@@ -17,12 +17,15 @@
 
 
 Route::group([], function() {
-   Route::get('/posts', 'API\PostsController@index')->name('posts.index');
-   Route::get('/posts/{post}', 'API\PostsController@show')->name('posts.show');
+   Route::get('/posts', 'API\PostsController@index')->name('api.posts.index');
+   Route::get('/posts/{post}', 'API\PostsController@show')->name('api.posts.show');
+   Route::post('/posts', 'API\PostsController@store')->name('api.posts.store');
+   Route::put('/posts/{post}', 'API\PostsController@update')->name('api.posts.update');
+   Route::delete('/posts/{post}', 'API\PostsController@destroy')->name('api.posts.destroy');
 
-   Route::get('/users', 'API\UsersController@index')->name('users.index');
-   Route::get('/users/{user}', 'API\UsersController@show')->name('users.show');
+   Route::get('/users', 'API\UsersController@index')->name('api.users.index');
+   Route::get('/users/{user}', 'API\UsersController@show')->name('api.users.show');
 
-   Route::get('/channels', 'API\ChannelsController@index')->name('channels.index');
-   Route::get('/channels/{channel}', 'API\ChannelsController@show')->name('channels.show');
+   Route::get('/channels', 'API\ChannelsController@index')->name('api.channels.index');
+   Route::get('/channels/{channel}', 'API\ChannelsController@show')->name('api.channels.show');
 });
